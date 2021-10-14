5fb15e490054fd01b74f9cba447c65c96e20ee9f
EC2 Default User <ec2-user@ip-172-31-47-224.us-east-2.compute.internal>
2021-10-14 05:41:14 +0000

    part 2, scenario 2 - update view, controller and route for scenario 2

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -4,6 +4,12 @@ class MoviesController < ApplicationController
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
+    @dirname = Movie.find(params[:id]).director
+    if @dirname == "" || @dirname == nil
+    @has_director = false
+    else
+    @has_director = true
+    end
   end
 
   def index
@@ -31,6 +37,11 @@ class MoviesController < ApplicationController
     redirect_to movie_path(@movie)
   end
 
+  def similar
+    dirname = Movie.find(params[:id]).director
+    @movies = Movie.with_director(dirname)
+  end
+
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
@@ -42,6 +53,6 @@ class MoviesController < ApplicationController
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
 end
--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -2,4 +2,5 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
+    match '/movies/similar/:id', to: 'movies#similar', as: 'similar_directors', via: :get
 end
