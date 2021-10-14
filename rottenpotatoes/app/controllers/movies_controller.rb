d0431b3eceec9c65ab1c022d37186614933a40bb
EC2 Default User <ec2-user@ip-172-31-47-224.us-east-2.compute.internal>
2021-10-14 08:48:11 +0000

    add hw 3 features

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -7,7 +7,26 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    sort = params[:sort] || session[:sort]
+    case sort
+    when 'title'
+      ordering,@title_header = {:title => :asc}, 'bg-warning hilite'
+    when 'release_date'
+      ordering,@date_header = {:release_date => :asc}, 'bg-warning hilite'
+    end
+    @all_ratings = Movie.all_ratings
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
+
+    if @selected_ratings == {}
+      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+    end
+
+    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    end    
+    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
     if @has_director = params[:has_director]
       @no_dir_movie = Movie.find(params[:id]).title
     end
@@ -55,6 +74,6 @@ class MoviesController < ApplicationController
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
+    params.require(:movie).permit(:title, :sort, :rating, :description, :release_date, :director)
   end
 end
--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    %w(G PG PG-13 NC-17 R)
+  end
     def self.with_director(dirname)
        Movie.where(:director => dirname)
     end
